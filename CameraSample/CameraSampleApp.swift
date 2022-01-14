e8a85f8db965874e3a185d98761dca9ad01648e7
cra1nbow <dev@cra1nbow.is>
2022-01-14 20:02:55 +0900

    Initial Commit

---
+++ CameraSample/CameraSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSampleApp.swift
+//  CameraSample
+//
+//  Created by cra1nbow on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct CameraSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
